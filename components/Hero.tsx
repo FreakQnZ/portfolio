6fa8b19ded5fed85b1f3c79872dbbe6ed7fc40dd
FreakQnZ <anuraag.srivatsa123@gmail.com>
2025-07-24 15:03:28 +0530

    Updated with new Projects

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,60 +1,73 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { Spotlight } from './ui/Spotlight'
-import { FlipWords } from './ui/flip-words'
-import { HoverBorderGradient } from './ui/hover-border-gradient'
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { FlipWords } from "./ui/flip-words";
+import { HoverBorderGradient } from "./ui/hover-border-gradient";
 
 const Hero = () => {
+  const scrolltofirst = () => {
+    const ele = document.getElementById("first");
+    ele?.scrollIntoView({ behavior: "smooth" });
+  };
 
-    const scrolltofirst = () => {
-        const ele = document.getElementById('first')
-        ele?.scrollIntoView({ behavior: 'smooth' })
-    }
+  return (
+    <div className=" text-white w-full flex flex-col items-center relative">
+      <div className=" w-full">
+        <Spotlight
+          className="top-20 2xl:-top-32 xl:top-0 lg:left-52 left-0"
+          fill="white"
+        />
+        <Spotlight
+          className=" top-48 -left-10 md:-left-32 md:top-40 md:h-[80vh] md:pt-40"
+          fill="skyblue"
+        />
+        <Spotlight className="left-full h-[30vh]" fill="beige" />
+      </div>
+      <div className="h-screen w-full bg-slate-950  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-slate-950 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      </div>
 
-    return (
-        <div className=' text-white w-full flex flex-col items-center relative'>
-            <div className=' w-full'>
-                <Spotlight className="top-20 2xl:-top-32 xl:top-0 lg:left-52 left-0" fill="white" />
-                <Spotlight className=' top-48 -left-10 md:-left-32 md:top-40 md:h-[80vh] md:pt-40' fill='skyblue' />
-                <Spotlight className='left-full h-[30vh]' fill='beige' />
-            </div>
-            <div className="h-screen w-full bg-slate-950  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-                <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-slate-950 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-            </div>
+      <div className=" z-20 flex flex-col items-center justify-between h-dvh p-2">
+        <p className="uppercase tracking-widest text-xs text-center">
+          Created With{" "}
+          <span className=" font-semibold">
+            Next.JS Tailwind CSS Framer Motion
+          </span>
+        </p>
 
+        <div>
+          <p className="uppercase tracking-widest text-center p-4 m-4 text-2xl lg:text-6xl">
+            Welcome to my
+          </p>
 
-            <div className=' z-20 flex flex-col items-center justify-between h-dvh p-2'>
-                <p className="uppercase tracking-widest text-xs text-center">
-                    Created With <span className=' font-semibold'>Next.JS Tailwind CSS Framer Motion</span>
-                </p>
-
-                <div>
-                    <p className='uppercase tracking-widest text-center p-4 m-4 text-2xl lg:text-6xl'>Welcome to my</p>
-
-                    <FlipWords words={['Portfolio', 'Blog', 'Diary', 'Space']} duration={2000} />
-                </div>
-
-                <div>
-                    <div className=' text-lg text-center m-4'>
-                        Hi I&apos;m Anuraag, A third year undergrad CSE - AIML student at PES University
-                    </div>
-
-                    <div className="flex justify-center text-center p-2">
-                        <HoverBorderGradient
-                            containerClassName="rounded-full"
-                            as="button"
-                            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
-                        >
-                            <span onClick={scrolltofirst} className='px-5'>Let&apos;s Go</span>
-                        </HoverBorderGradient>
-                    </div>
-                </div>
-            </div>
+          <FlipWords
+            words={["Portfolio", "Blog", "Diary", "Space"]}
+            duration={2000}
+          />
         </div>
-    )
-}
 
+        <div>
+          <div className=" text-lg text-center m-4">
+            Hi I&apos;m Anuraag, A fourth year undergrad CSE - AIML student at
+            PES University
+          </div>
 
+          <div className="flex justify-center text-center p-2">
+            <HoverBorderGradient
+              containerClassName="rounded-full"
+              as="button"
+              className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+            >
+              <span onClick={scrolltofirst} className="px-5">
+                Let&apos;s Go
+              </span>
+            </HoverBorderGradient>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- components/section1.tsx
+++ components/section1.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useState, useEffect } from 'react'
-import React from 'react'
-import Image from 'next/image';
-import { PinContainer } from './ui/3d-pin';
-import { BackgroundBeams } from './ui/background-beams';
+import { useState, useEffect } from "react";
+import React from "react";
+import Image from "next/image";
+import { PinContainer } from "./ui/3d-pin";
+import { BackgroundBeams } from "./ui/background-beams";
 
 // const Section1 = () => {
 
@@ -125,105 +125,141 @@ import { BackgroundBeams } from './ui/background-beams';
 // }
 
 const AnimatedPinDemo = () => {
+  const [isClient, setIsClient] = useState(false);
 
-    const [isClient, setIsClient] = useState(false)
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-    useEffect(() => {
-        setIsClient(true)
-    }, [])
+  const items = [
+    {
+      title: "Tutor",
+      description: "AI pipeline for processing videos and PDFs using Kafka.",
+      placeholder: "Github Link",
+      href: "https://github.com/FreakQnZ/Tutor",
+      img: "/architecture.png",
+      imgClass: "pt-10 pl-10",
+      width: 200,
+    },
+    {
+      title: "Split",
+      description:
+        "Next js App to add bills, split them among friends and keep track of expenses.",
+      placeholder: "Site Link",
+      href: "https://expensesplit.vercel.app/",
+      img: "/split.png",
+      imgClass: "pt-10",
+      width: 300,
+    },
+    {
+      title: "Samvaada",
+      description:
+        "Agentic AI App to Communicate with Enterprise Database using LangGraph",
+      placeholder: "Github Link",
+      href: "https://github.com/FreakQnZ/Samvaada",
+      img: "/samvaada.png",
+      imgClass: "pt-10",
+      width: 300,
+    },
+    {
+      title: "Anony Chat",
+      description: "Chat App with Websockets for anonymous chating in rooms",
+      placeholder: "Site Link",
+      href: "https://anonychat-black.vercel.app/",
+      img: "/chat.png",
+      imgClass: "pt-10",
+      width: 300,
+    },
+    {
+      title: "Robin Internships",
+      description:
+        "Platofrom originally created for handling College fest student internships.",
+      placeholder: "Github Link",
+      href: "https://github.com/FreakQnZ/robin-internships",
+      img: "/robin.png",
+      imgClass: "pt-10",
+      width: 300,
+    },
+    {
+      title: "Task Manager",
+      description:
+        "Task Manager web application with auth using clerk and a responsive and unique design.",
+      placeholder: "Github Link",
+      href: "https://github.com/FreakQnZ/Task-Manager-Web-Application",
+      img: "/taskmanager.png",
+      imgClass: "pt-6",
+      width: 300,
+    },
+    {
+      title: "Auth Module",
+      description:
+        "Auth module made from scratch using JWT, includes signup login and forgot password feature",
+      placeholder: "Github Link",
+      href: "https://github.com/FreakQnZ/Auth-Module",
+      img: "/jwt.png",
+      imgClass: "",
+      width: 300,
+    },
+    {
+      title: "Churn Prediction",
+      description:
+        "Using Tensorflow to create a Deep Learning Model to predict Customer Loyalty",
+      placeholder: "Github Link",
+      href: "https://github.com/FreakQnZ/Neural-Network-Project",
+      img: "/churn.png",
+      imgClass: "pt-6 pl-10",
+      width: 200,
+    },
+    {
+      title: "And Much More on my Github",
+      description:
+        "Check out my Github for more projects and projects I am working on.",
+      placeholder: "Github Link",
+      href: "https://github.com/FreakQnZ",
+      img: "/github.png",
+      imgClass: "pt-6 pl-16",
+      width: 180,
+    },
+    // {
+    //     title: "The Spirit of Adventure",
+    //     description: "Embark on exciting journeys and thrilling discoveries.",
+    //     header: <Skeleton />,
+    //     icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+    // },
+  ];
 
-    const items = [
-        {
-            title: "Auth Module",
-            description: "Auth module made from scratch using JWT, includes signup login and forgot password feature",
-            placeholder: "Github Link",
-            href: "https://github.com/FreakQnZ/Auth-Module",
-            img: "/jwt.png",
-            imgClass: "",
-            width: 300
-        },
-        {
-            title: "Robin Internships",
-            description: "Platofrom originally created for handling College fest student internships.",
-            placeholder: "Live Site Link",
-            href: "https://robinit.in/",
-            img: "/robin.png",
-            imgClass: "pt-10",
-            width: 300
-        },
-        {
-            title: "Task Manager",
-            description: "Task Manager web application with auth using clerk and a responsive and unique design.",
-            placeholder: "Live Site Link",
-            href: "https://task-manager-web-application-nine.vercel.app/",
-            img: "/taskmanager.png",
-            imgClass: "pt-6",
-            width: 300
-        },
-        {
-            title: "Anonymous Chat Room App",
-            description: "Chat App with Websockets for anonymous chating in rooms",
-            placeholder: "Github Link",
-            href: "https://github.com/FreakQnZ/CN-Project---Chat-app-with-WebSockets",
-            img: "/jwt.png",
-            imgClass: "",
-            width: 300
-        },
-        {
-            title: "Churn Prediction",
-            description: "Using Tensorflow to create a Deep Learning Model to predict Customer Loyalty",
-            placeholder: "Github Link",
-            href: "https://github.com/FreakQnZ/Neural-Network-Project",
-            img: "/churn.png",
-            imgClass: "pt-6 pl-10",
-            width: 200
-        },
-        {
-            title: "And Much More on my Github",
-            description: "Check out my Github for more projects and projects I am working on.",
-            placeholder: "Github Link",
-            href: "https://github.com/FreakQnZ",
-            img: "/github.png",
-            imgClass: "pt-6 pl-16",
-            width: 180
-        },
-        // {
-        //     title: "The Spirit of Adventure",
-        //     description: "Embark on exciting journeys and thrilling discoveries.",
-        //     header: <Skeleton />,
-        //     icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-        // },
-    ];
+  return isClient ? (
+    <section
+      id="first"
+      className=" w-full bg-slate-950 flex flex-col items-center justify-between"
+    >
+      <div className="text-4xl text-center p-12">Projects</div>
+      <div className=" relative w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 items-center justify-center gap-12 pb-10">
+        {items.map((item, i) => (
+          // <div key={i} className=''>
 
-    return (
-        isClient ?
-            <section id='first' className=' w-full bg-slate-950 xl:h-screen flex flex-col items-center justify-between'>
-                <div className='text-4xl text-center p-12'>Projects</div>
-                <div className=" relative w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 items-center justify-center gap-12 pb-10">
-                    {items.map((item, i) => (
-                        // <div key={i} className=''>
-
-                        <PinContainer
-                            key={i}
-                            title={item?.placeholder}
-                            href={item?.href}
-                        >
-                            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-                                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                                    {item.title}
-                                </h3>
-                                <div className="text-base !m-0 !p-0 font-normal">
-                                    <span className="text-slate-500 ">
-                                        {item.description}
-                                    </span>
-                                </div>
-                                <div className={`${item.imgClass}`}><Image src={item.img} height={200} alt="img" width={item.width} /></div>
-                                {/* <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" /> */}
-                            </div>
-                        </PinContainer>
-                        // </div>
-                    ))}
-                    {/* <PinContainer
+          <PinContainer key={i} title={item?.placeholder} href={item?.href}>
+            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+              <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                {item.title}
+              </h3>
+              <div className="text-base !m-0 !p-0 font-normal">
+                <span className="text-slate-500 ">{item.description}</span>
+              </div>
+              <div className={`${item.imgClass}`}>
+                <Image
+                  src={item.img}
+                  height={200}
+                  alt="img"
+                  width={item.width}
+                />
+              </div>
+              {/* <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" /> */}
+            </div>
+          </PinContainer>
+          // </div>
+        ))}
+        {/* <PinContainer
                         title="/ui.aceternity.com"
                         href="https://twitter.com/mannupaaji"
                     >
@@ -319,10 +355,9 @@ const AnimatedPinDemo = () => {
                             <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
                         </div>
                     </PinContainer> */}
-                </div>
-            </section>
-            : null
-    );
-}
+      </div>
+    </section>
+  ) : null;
+};
 
-export default AnimatedPinDemo
+export default AnimatedPinDemo;
